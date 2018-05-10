7aeea81b737d23a749f838495d92fd4bb77d9b35
beimenchuixue <jia2jiayuan@163.com>
2018-05-10 10:13:08 +0800

    创建职位信息表 Job

--- worker/models.py
+++ worker/models.py
@@ -2,6 +2,29 @@ from django.db import models
 from datetime import  datetime
 
 
+class Job(models.Model):
+    """
+    职位信息表
+    """
+    IS_CORE = (
+        (0, '不是'),
+        (1, '是')
+    )
+    name = models.CharField(max_length=20, verbose_name="职位名")
+    description = models.TextField(verbose_name="工作描叙")
+    expect_salary = models.DecimalField(verbose_name="预估工资")
+    is_core = models.IntegerField(choices=IS_CORE, default=0, verbose_name="是否是核心职位")
+
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加记录时间")
+
+    class Meta:
+        verbose_name = "职位信息"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
 class Worker(models.Model):
     """
     职工信息表
@@ -22,6 +45,14 @@ class Worker(models.Model):
         ('graduate', '研究生'),
         ('doctorate', '博士')
     )
+    IS_CORE = (
+        (0, '不是'),
+        (1, '是')
+    )
+    IS_CHARGE = (
+        (0, '不是'),
+        (1, '是')
+    )
     name = models.CharField(max_length=20, verbose_name="姓名")
     sex = models.CharField(max_length=6, choices=SEX,default="male", verbose_name="性别")
     birthday = models.DateField(verbose_name="出生日期")
@@ -34,8 +65,11 @@ class Worker(models.Model):
     school = models.CharField(max_length=50,null=True, blank=True, verbose_name="毕业院校")
     work_status = models.IntegerField(choices=WORK_STATUS, default=0, verbose_name="在职状态")
     join_time = models.DateTimeField(verbose_name="入职时间")
-    comment = models.ForeignKey()
-    department = models.ForeignKey()
+    is_core = models.IntegerField(choices=IS_CORE, default=0, verbose_name="是否是核心成员")
+    is_charge = models.IntegerField(choices=IS_CHARGE, default=0, verbose_name="是否主负责人")
+    job = models.ForeignKey(Job, on_delete=models.CASCADE, verbose_name="职位")
+    comment = models.ForeignKey(verbose_name="员工评价")
+    department = models.ForeignKey(verbose_name="所属部门")
 
     add_time = models.DateTimeField(default=datetime.now, verbose_name="添加记录时间")
 
