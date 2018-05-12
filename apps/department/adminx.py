e37993992fc3a2fd911db20a7f73f735f949b190
beimenchuixue <jia2jiayuan@163.com>
2018-05-12 14:09:21 +0800

    完成 department app中表后台注册

--- apps/department/adminx.py
+++ apps/department/adminx.py
@@ -1,18 +1,27 @@
+from .models import Job, Department
+
 import xadmin
 from xadmin import views
 
-class Admin(object):
-    list_display = []
-    search_fields = []
-    list_filter = []
-    list_editable = []
-    model_icon = ''
 
-class Admin(object):
-    list_display = []
-    search_fields = []
-    list_filter = []
-    list_editable = []
-    model_icon = ''
+class JobAdmin(object):
+    list_display = ['name', 'description', 'expect_salary', 'is_core', 'worker_num', 'add_time']
+    search_fields = ['name', 'description', 'expect_salary', 'is_core', 'worker_num']
+    list_filter = ['name', 'description', 'expect_salary', 'is_core', 'add_time']
+    list_editable = ['name', 'description', 'expect_salary', 'is_core', 'add_time']
+    style_fields = {"description": "ueditor"}
+    model_icon = 'fa fa-tags'
 
-xadmin.site.register(, Admin)
+
+class DepartmentAdmin(object):
+    list_display = ['name', 'duty', 'description', 'create_time', 'vision','worker_num', 'add_time',]
+    search_fields = ['name', 'duty', 'description', 'create_time', 'vision', 'worker_num']
+    list_filter = ['name', 'duty', 'description', 'create_time', 'vision', 'add_time']
+    list_editable = ['name', 'duty', 'description', 'create_time', 'vision', 'add_time']
+    # readonly_fields = ['get_worker_num']
+    style_fields = {"description": "ueditor", "vision": "ueditor"}
+    model_icon = 'fa fa-building'
+
+
+xadmin.site.register(Department, DepartmentAdmin)
+xadmin.site.register(Job, JobAdmin)
